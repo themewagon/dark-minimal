e7a9e1de17970e3ff3bf05fa0e287950a87672e9
Pipul Boddyo <pipul.boddyo@gmail.com>
2026-09-08 14:49:08 +0600

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://themewagon.github.io",
+  base: "/dark-minimal",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
